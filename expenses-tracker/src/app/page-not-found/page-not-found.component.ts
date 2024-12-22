91d55ef5c77d70cfd8f4ec511bbcb371af79f47e
Louis GINDRE <louis.gindre@edu.devinci.fr>
2024-12-22 14:09:25 +0100

    Navbar creation

--- expenses-tracker/src/app/page-not-found/page-not-found.component.ts
+++ expenses-tracker/src/app/page-not-found/page-not-found.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-page-not-found',
-  imports: [],
+  imports: [
+    RouterLink
+  ],
   templateUrl: './page-not-found.component.html',
+  standalone: true,
   styles: ``
 })
 export class PageNotFoundComponent {
